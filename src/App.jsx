88d2cc884148c858b2109a549d832f7515b94712
falconcodes <Prathamesh-Chavan-232>
2022-11-15 19:55:10 +0530

    wysiwyg Text editor Setup & UI

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./css/App.css";
+import { useState } from "react";
+import TextEditor from "./QuillEditor";
+import QuillEditor from "./QuillEditor";
+
+function App() {
+  // const [count, setCount] = useState(0);
+  return <QuillEditor />;
+}
+
+export default App;
---
+++ src/QuillEditor.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import "./css/Quill.css";
+import Quill from "quill";
+import { useEffect, useRef } from "react";
+import "quill/dist/quill.snow.css";
+
+export default function QuillEditor() {
+  const wrapperRef = useRef();
+
+  var TOOLBAR_OPTIONS = [
+    [{ header: [1, 2, 3, 4, 5, 6, false] }],
+    [{ font: [] }],
+    [{ list: "ordered" }, { list: "bullet" }],
+    ["bold", "italic", "underline"],
+    [{ color: [] }, { background: [] }],
+  ];
+  var OPTIONS = [
+    ["bold", "italic", "underline", "strike"], // toggled buttons
+    ["blockquote", "code-block"],
+
+    [{ header: 1 }, { header: 2 }], // custom button values
+    [{ list: "ordered" }, { list: "bullet" }],
+    [{ script: "sub" }, { script: "super" }], // superscript/subscript
+    [{ indent: "-1" }, { indent: "+1" }], // outdent/indent
+    [{ direction: "rtl" }], // text direction
+
+    [{ size: ["small", false, "large", "huge"] }], // custom dropdown
+    [{ header: [1, 2, 3, 4, 5, 6, false] }],
+
+    [{ color: [] }, { background: [] }], // dropdown with defaults from theme
+    [{ font: [] }],
+    [{ align: [] }],
+
+    ["clean"], // remove formatting button
+  ];
+
+  useEffect(() => {
+    const editor = document.createElement("div");
+    wrapperRef.current.append(editor);
+    new Quill(editor, {
+      modules: { toolbar: TOOLBAR_OPTIONS },
+      theme: "snow",
+      // placeholder: "Compose and epic...",
+    });
+
+    return () => {
+      wrapperRef.innerHTML = "";
+    };
+  }, []);
+
+  return <div className="container" ref={wrapperRef}></div>;
+}
